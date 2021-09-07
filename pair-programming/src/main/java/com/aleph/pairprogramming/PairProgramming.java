be45023df0291392e3b7c21fdb179348b0ad4148
deiyanfott <33542094+deiyanfott@users.noreply.github.com>
2021-09-07 11:34:56 +0800

    Initial commit

---
+++ pair-programming/src/main/java/com/aleph/pairprogramming/PairProgramming.java
@@ -0,0 +1,7 @@
+package com.aleph.pairprogramming;
+
+public class PairProgramming {
+	public static void myMethod() {
+		
+	}
+}
---
+++ pair-programming/src/test/java/com/aleph/pairprogramming/PairProgrammingTest.java
@@ -0,0 +1,14 @@
+package com.aleph.pairprogramming;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PairProgrammingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
